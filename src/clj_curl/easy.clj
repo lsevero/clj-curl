249d489f5a67fa88f01df65c1434ad597b166465
Lucas Severo <lvspais2@gmail.com>
2019-09-23 03:01:38 -0300

    added getinfo functions

--- src/clj_curl/easy.clj
+++ src/clj_curl/easy.clj
@@ -1,5 +1,6 @@
 (ns clj-curl.easy
   (:import [com.sun.jna NativeLibrary Pointer]
+           [com.sun.jna.ptr PointerByReference DoubleByReference LongByReference]
            [clj_curl.Handlers MemHandler FileHandler]))
 
 (def ^:private libcurl (com.sun.jna.NativeLibrary/getInstance "curl"))
@@ -12,12 +13,46 @@
   (.invoke (.getFunction libcurl "curl_easy_init") Pointer (to-array [])))
 
 (defn setopt ^Integer
-  [^Pointer curl ^Integer i ^Object param]
-  (.invoke (.getFunction libcurl "curl_easy_setopt") Integer (to-array [curl i param])))
+  [^Pointer curl ^Integer opt param]
+  (.invoke (.getFunction libcurl "curl_easy_setopt") Integer (to-array [curl opt param])))
 
 (defn perform ^Integer
   [^Pointer curl]
   (.invoke (.getFunction libcurl "curl_easy_perform") Integer (to-array [curl])))
+
+(defn cleanup
+  [^Pointer curl]
+  (.invoke (.getFunction libcurl "curl_easy_cleanup") Void/TYPE (to-array [])))
+
+(defn getinfo ^Integer
+  [^Pointer curl ^Integer opt param]
+  (.invoke (.getFunction libcurl "curl_easy_getinfo") Integer (to-array [curl opt param])))
+
+(defn getinfo-double ^Double
+  [^Pointer curl ^Integer opt]
+  (let [p-double (DoubleByReference.)]
+    (do
+      (.invoke (.getFunction libcurl "curl_easy_getinfo") Integer (to-array [curl opt p-double]))
+      (.getValue p-double))))
+
+(defn getinfo-long ^Long
+  [^Pointer curl ^Integer opt]
+  (let [p-long (LongByReference.)]
+    (do
+      (.invoke (.getFunction libcurl "curl_easy_getinfo") Integer (to-array [curl opt p-long]))
+      (.getValue p-long))))
+
+(defn getinfo-string ^String
+  [^Pointer curl ^Integer opt]
+  (let [p-str (PointerByReference.)]
+    (do
+      (.invoke (.getFunction libcurl "curl_easy_getinfo") Integer (to-array [curl opt p-str]))
+      (let [value (.getValue p-str)]
+        (if (= value Pointer/NULL) "" value)))))
+
+(defn duphandle
+  [^Pointer curl]
+  (.invoke (.getFunction libcurl "curl_easy_duphandle") Pointer (to-array [curl])))
 
 (def c (init))
 (def m (clj_curl.Handlers.MemHandler.))
