593aa6bdefd9d8204605c68b752b6adef9cd830c
Wanderson Ferreira <wanderson.ferreira@captalys.com.br>
2019-09-24 01:03:34 -0300

    removing source-paths from project.clj

--- project.clj
+++ project.clj
@@ -10,7 +10,6 @@
                    :plugins [[cider/cider-nrepl "0.22.3"]
                              [lein-cloverage "1.1.1"]]
                    :source-paths ["src" "test" "examples"]}
-             :test {:dependencies [[org.clojure/clojure "1.10.0"]]
-                    :source-paths ["test"]}}
+             :test {:dependencies [[org.clojure/clojure "1.10.0"]]}}
   :main main
   :repl-options {:init-ns clj-curl.easy})
