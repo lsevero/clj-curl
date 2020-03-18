a16dc30df4f10069db37b538dc72989f24f77cdf
Lucas Severo <lucas.severo@captalys.com.br>
2020-03-18 15:23:52 -0300

    incremented the project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-curl "1.0.0"
+(defproject clj-curl "1.1.0"
   :description "curl for clojure"
   :url "https://github.com/lsevero/clj-curl"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -9,6 +9,8 @@
         clj-curl.Exceptions]
   :profiles {:dev {:plugins [[cider/cider-nrepl "0.22.3"]
                              [lein-cloverage "1.1.1"]]
+                   
+                   :global-vars {*warn-on-reflection* true}
                    :main main
                    :repl-options {:init-ns clj-curl.easy}
                    :source-paths ["src" "test" "examples"]}
